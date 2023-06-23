3b7775e1a27b5e74914aae0da3b66cd658c4e951
JigSaw <108606727+luvBB@users.noreply.github.com>
2023-06-23 15:48:29 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,513 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from sopel.plugin import find
+
+@find(r"Adjud")
+def x1(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Vrancea")
+
+@find(r"Aiud")
+def x2(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Alba")
+
+@find(r"Alba Iulia")
+def x3(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Alba")
+
+@find(r"Alexandria")
+def x4(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Teleorman")
+
+@find(r"Arad")
+def x5(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Arad")
+
+@find(r"Bacau")
+def x6(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Bacau")
+
+@find(r"Baia Mare")
+def x7(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Maramures")
+
+@find(r"Bailesti")
+def x8(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Dolj")
+
+@find(r"Barlad")
+def x9(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Vaslui")
+
+@find(r"Beius")
+def x10(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Bihor")
+
+@find(r"Bistrita")
+def x11(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Bistrita Nasaud")
+
+@find(r"Blaj")
+def x12(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Alba")
+
+@find(r"Botosani")
+def x13(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Botosani")
+
+@find(r"Brad")
+def x14(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Hunedoara")
+
+@find(r"Brasov")
+def x15(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Brasov")
+
+@find(r"Braila")
+def x16(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Braila")
+
+@find(r"Buzau")
+def x17(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Buzau")
+
+@find(r"Calafat")
+def x18(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Dolj")
+
+@find(r"Caracal")
+def x19(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Olt")
+
+@find(r"Caransebes")
+def x20(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Caras Severin")
+
+@find(r"Carei")
+def x21(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Satu Mare")
+
+@find(r"Calarasi")
+def x22(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Calarasi")
+
+@find(r"Campia Turzii")
+def x23(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Cluj")
+
+@find(r"Campina")
+def x24(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Prahova")
+
+@find(r"Campulung Moldovenesc")
+def x25(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Suceava")
+
+@find(r"Campulung Muscel")
+def x26(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Arges")
+
+@find(r"Cluj Napoca")
+def x27(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Cluj")
+
+@find(r"Codlea")
+def x28(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Brasov")
+
+@find(r"Constanta")
+def x29(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Constanta")
+
+@find(r"Craiova")
+def x30(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Dolj")
+
+@find(r"Curtea de Arges")
+def x31(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Arges")
+
+@find(r"Dej")
+def x32(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Cluj")
+
+@find(r"Deva")
+def x33(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Hunedoara")
+
+@find(r"Dorohoi")
+def x34(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Botosani")
+
+@find(r"Dragasani")
+def x35(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Valcea")
+
+@find(r"Drobeta Turnu Severin")
+def x36(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Mehedinti")
+
+@find(r"Fagaras")
+def x37(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Brasov")
+
+@find(r"Falticeni")
+def x38(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Suceava")
+
+@find(r"Fetesti")
+def x39(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Ialomita")
+
+@find(r"Focsani")
+def x40(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Vrancea")
+
+@find(r"Galati")
+def x41(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Galati")
+
+@find(r"Gheorgheni")
+def x42(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Harghita")
+
+@find(r"Gherla")
+def x43(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Cluj")
+
+@find(r"Giurgiu")
+def x44(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Giurgiu")
+
+@find(r"Hunedoara")
+def x45(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Hunedoara")
+
+@find(r"Husi")
+def x46(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Vaslui")
+
+@find(r"Iasi")
+def x47(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Iasi")
+
+@find(r"Lugoj")
+def x48(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Timis")
+
+@find(r"Lupeni")
+def x49(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Hunedoara")
+
+@find(r"Mangalia")
+def x50(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Constanta")
+
+@find(r"Marghita")
+def x51(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Bihor")
+
+@find(r"Medgidia")
+def x52(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Constanta")
+
+@find(r"Medias")
+def x53(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Sibiu")
+
+@find(r"Miercurea Ciuc")
+def x54(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Hargita")
+
+@find(r"Moinesti")
+def x55(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Bacau")
+
+@find(r"Moreni")
+def x56(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Dambovita")
+
+@find(r"Motru")
+def x57(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Gorj")
+
+@find(r"Odorheiu Secuiesc")
+def x58(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Harghita")
+
+@find(r"Oltenita")
+def x59(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Calarasi")
+
+@find(r"Onesti")
+def x60(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Bacau")
+
+@find(r"Oradea")
+def x61(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Bihor")
+
+@find(r"Orastie")
+def x62(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Hunedoara")
+
+@find(r"Orsova")
+def x63(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Mehedinti")
+
+@find(r"Pascani")
+def x64(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Iasi")
+
+@find(r"Petrosani")
+def x65(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Hunedoara")
+
+@find(r"Piatra Neamt")
+def x66(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Neamt")
+
+@find(r"Pitesti")
+def x67(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Arges")
+
+@find(r"Ploiesti")
+def x68(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Prahova")
+
+@find(r"Radauti")
+def x69(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Suceava")
+
+@find(r"Ramnicu Sarat")
+def x70(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Buzau")
+
+@find(r"Ramnicu Valcea")
+def x71(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Valcea")
+
+@find(r"Reghin")
+def x72(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Mures")
+
+@find(r"Resita")
+def x73(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Caras Severin")
+
+@find(r"Roman")
+def x74(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Neamt")
+
+@find(r"Rosiorii de Vede")
+def x75(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Teleorman")
+
+@find(r"Salonta")
+def x76(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Bihor")
+
+@find(r"Satu Mare")
+def x77(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Satu Mare")
+
+@find(r"Sacele")
+def x78(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Brasov")
+
+@find(r"Sebes")
+def x79(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Alba")
+
+@find(r"Sfantu Gheorghe")
+def x80(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Covasna")
+
+@find(r"Sibiu")
+def x81(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Sibiu")
+
+@find(r"Sighetu Marmatiei")
+def x82(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Maramures")
+
+@find(r"Sighisoara")
+def x83(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Mures")
+
+@find(r"Slatina")
+def x84(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Olt")
+
+@find(r"Slobozia")
+def x85(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Ialomita")
+
+@find(r"Suceava")
+def x86(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Suceava")
+
+@find(r"Targoviste")
+def x87(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Dambovita")
+
+@find(r"Targu Jiu")
+def x88(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Gorj")
+
+@find(r"Targu Mures")
+def x89(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Mures")
+
+@find(r"Targu Secuiesc")
+def x90(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Covasna")
+
+@find(r"Tarnaveni")
+def x91(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Mures")
+
+@find(r"Tecuci")
+def x92(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Galati")
+
+@find(r"Timisoara")
+def x93(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Timis")
+
+@find(r"Toplita")
+def x94(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Harghita")
+
+@find(r"Tulcea")
+def x95(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Tulcea")
+
+@find(r"Turda")
+def x96(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Cluj")
+
+@find(r"Turnu Magurele")
+def x97(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Teleorman")
+
+@find(r"Urziceni")
+def x98(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Ialomita")
+
+@find(r"Vaslui")
+def x99(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Vaslui")
+
+@find(r"Vatra Dornei")
+def x100(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Suceava")
+
+@find(r"Vulcan")
+def x101(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Hunedoara")
+
+@find(r"Zalau")
+def x102(bot, trigger):
+    if trigger.nick == "droops16":
+        bot.say(r"Salaj")
